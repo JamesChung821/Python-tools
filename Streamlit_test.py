2914856ee423f10075b898caaa8c41ac2b80921d
Cheng-Chu Chung <cheng-chu.chung@stonybrook.edu>
2022-08-10 09:55:45 -0400

    column with selectbox

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -23,18 +23,16 @@ def main():
 
     st.subheader('Data Visualization with respect to Survived')
     left_column, right_column = st.columns(2)
-    # with left_column:
-    #    'Numerical Plot'
-    #     num_feat = st.selectbox(
-    #    'Select Numerical Feature', df.select_dtypes('number').columns)
-    #     fig = px.histogram(df, x = num_feat, color = 'survived')
-    #     st.plotly_chart(fig, use_container_width=True)
-    # with right_column:
-    #    'Categorical column'
-    #     cat_feat = st.selectbox(
-    #     'Select Categorical Feature', df.select_dtypes(exclude =   'number').columns)
-    #     fig = px.histogram(df, x =cat_feat, color = 'survived' )
-    # st.plotly_chart(fig, use_container_width=True)
+    with left_column:
+        'Numerical Plot'
+        num_feat = st.selectbox('Select Numerical Feature', df.select_dtypes('number').columns)
+        fig = px.histogram(df, x=num_feat, color='survived')
+        st.plotly_chart(fig, use_container_width=True)
+    with right_column:
+        'Categorical column'
+        cat_feat = st.selectbox('Select Categorical Feature', df.select_dtypes(exclude='number').columns)
+        fig = px.histogram(df, x=cat_feat, color='survived')
+        st.plotly_chart(fig, use_container_width=True)
 
     st.subheader('Map nearby Stony Brook')
     number_of_points = 100
