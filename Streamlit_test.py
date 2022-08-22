9340454f23123e58c300b256ced630bf1a7a4fc0
Cheng-Chu Chung <cheng-chu.chung@stonybrook.edu>
2022-08-22 02:17:22 -0400

    test data and picture update

--- Streamlit_test.py
+++ Streamlit_test.py
@@ -10,6 +10,7 @@ import time
 import random
 from collections import defaultdict
 from sklearn import preprocessing, model_selection, tree, decomposition, ensemble, cluster, neighbors
+from PIL import Image
 
 # Constant
 PALETTE = pld.Spectral_4_r  # _r if you want to reverse the color sequence
@@ -21,6 +22,11 @@ DEGREES = [1]		# PolynomialFeatures
 DATA_SIZE = 800
 N_CLUSTERS = DATA_SIZE//100
 VISUALIZATION = False
+AGE = ['11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '70+']
+GENDER = ['Male', 'Female', 'Third gender']
+GOAL = ['Date', 'Business', 'Friends', 'Family', 'Alone', 'Other']
+HOURS = ['Brunch', 'Lunch', 'Dinner', 'Anytime']
+DISTANCE = ['0-25', '26-50', '51-100', 'idc']
 
 
 def main():
@@ -33,39 +39,63 @@ def main():
 
     st.subheader('Train Dataset')
     # st.dataframe(df.describe())
-    train_data, label_data = data_preprocess(synthetic_dataset(), mode='Train')  # Extract data and labels
-    column_dimension = train_data.shape[1]
+    train_data, label_data, training_dataframe = data_preprocess(synthetic_dataset(), mode='Train')  # Extract data and labels
     st.dataframe(train_data)
 
     st.subheader("User's Input")
     test_data = user_input_feature()
+    column_dimension = test_data.shape[1]
     st.dataframe(test_data)
 
     st.subheader("Test Dataset")
-    test_data = data_preprocess(test_data, mode='Test')
+    test_data = data_preprocess(test_data, mode='Test', training_dataframe=training_dataframe)
     st.dataframe(test_data)
 
-    # standardizier = preprocessing.StandardScaler()  # Call a standardization object
-    # x_train = standardizier.fit_transform(train_data)  # Do the standardization
-    # x_test = standardizier.transform(test_data)  # Do 'transform' only on testing data
-    #
-    # pca = decomposition.PCA(n_components=column_dimension)  # Data compression
-    # x_train_pca = pca.fit_transform(x_train)
-    # x_test_pca = pca.transform(x_test)
-    # k_means = cluster.KMeans(n_clusters=N_CLUSTERS)  # <-------------------------------- number of clusters
-    # k_means.fit(x_train_pca)  # No labels
-    # # print('Cluster center data:', k_means.cluster_centers_)
-    # print('====================================')
-    # print('Cluster label')
-    # print(k_means.labels_)
-    # # print(k_means.get_params())
-    #
-    # predict_label = k_means.predict(x_test_pca)
-    # print('====================================')
-    # print('Predict label')
-    # print(predict_label)
-    # print('Data format of each data')
-    # print(x_test_pca[0])
+    standardizier = preprocessing.StandardScaler()  # Call a standardization object
+    x_train = standardizier.fit_transform(train_data)  # Do the standardization
+    x_test = standardizier.transform(test_data)  # Do 'transform' only on testing data
+
+    pca = decomposition.PCA(n_components=column_dimension)  # Data compression
+    x_train_pca = pca.fit_transform(x_train)
+    x_test_pca = pca.transform(x_test)
+    k_means = cluster.KMeans(n_clusters=N_CLUSTERS)  # <-------------------------------- number of clusters
+    k_means.fit(x_train_pca)  # No labels
+    # print('Cluster center data:', k_means.cluster_centers_)
+    print('====================================')
+    print('Cluster label')
+    print(k_means.labels_)
+    # print(k_means.get_params())
+
+    predict_label = k_means.predict(x_test_pca)
+    print('====================================')
+    print('Predict label')
+    print(predict_label)
+    print('Data format of each data')
+    print(x_test_pca[0])
+
+    train_dict = defaultdict(list)
+    for index, cluster_label in enumerate(k_means.labels_):
+        train_dict[f'{cluster_label}'].append(x_train_pca[index])
+
+    for index, test_label in enumerate(predict_label):
+        distance_array = np.array([])
+        for train_data_point in train_dict[f'{test_label}']:
+            distance = train_data_point - predict_label[index]
+            norm = np.linalg.norm(distance)
+            distance_array = np.append(distance_array, norm)
+        print('====================================')
+        print(f'Predict data {index} is in Group {test_label}')
+        print('Best data point:', (distance_array.argmin(), np.min(distance_array)))
+        print(label_data[distance_array.argmin()])
+        st.subheader('The Best Restaurant for you')
+        image = ''
+        if f'{label_data[distance_array.argmin()]}' == 'Shake Shack':
+            image = Image.open("D:\Research data\SSID\Advanced Computer Python\Python-tools\Shack Shack.png")
+        elif f'{label_data[distance_array.argmin()]}' == 'Burger King':
+            image = Image.open("D:\Research data\SSID\Advanced Computer Python\Python-tools\Burger King.png")
+        elif f'{label_data[distance_array.argmin()]}' == "McDonald's":
+            image = Image.open("D:\Research data\SSID\Advanced Computer Python\Python-tools\McDonalds.png")
+        st.image(image, caption=f'{label_data[distance_array.argmin()]}')
 
     st.subheader('Data Visualization with respect to Survived')
     # left_column, right_column = st.columns(2)
@@ -176,7 +206,7 @@ def synthetic_dataset():
     return pd.DataFrame(selection_dictionary)
 
 
-def data_preprocess(filename, mode='Train', training_data=None):
+def data_preprocess(filename, mode='Train', training_dataframe=None):
     """
     :param filename: str, the filename to be read into pandas
     :param mode: str, indicating the mode we are using (either Train or Test)
@@ -200,8 +230,8 @@ def data_preprocess(filename, mode='Train', training_data=None):
         labels = data['Restaurant']  # Save labels
         data.pop('Restaurant')
         data.pop('Extra column')
-        dataframe_format = data
-        return data, labels
+        dataframe_format = data.drop(data.index[1:], axis=0).replace(1, 0)
+        return data, labels, dataframe_format
 
     elif mode == 'Test':
         column_names = ['Age', 'Gender', 'Goal', 'Hours', 'Distance']
@@ -211,11 +241,11 @@ def data_preprocess(filename, mode='Train', training_data=None):
         column_dictionary['Goal'] = ['Date', 'Business', 'Friends', 'Family', 'Alone', 'Other']
         column_dictionary['Hours'] = ['Brunch', 'Lunch', 'Dinner', 'Anytime']
         column_dictionary['Distance'] = ['0-25', '26-50', '51-100', 'idc']
-        for column in column_dictionary:
-            for index in range(len(column_dictionary[column])):
-                data.loc[data[column] == column_dictionary[column][index], column] = index
-
-        dataframe_format = dataframe_format.drop(dataframe_format.index[1:]).replace(1, 0)
+        for column_index, column in enumerate(column_dictionary):
+            for item_index, item in enumerate(column_dictionary[column]):
+                # data.loc[data[column] == column_dictionary[column][item_index], column] = item_index
+                training_dataframe.loc[data[column] == item, f'{column}_{item}'] = 1
+        data = training_dataframe    # Store data using training dataframe to fit the feature dimension
         return data
 
 
