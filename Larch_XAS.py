80a526aae9c713cdee1e0d0b532a823202ace7c1
Cheng-Chu Chung <cheng-chu.chung@stonybrook.edu>
2022-09-03 16:49:29 -0400

    plot config

--- Larch_XAS.py
+++ Larch_XAS.py
@@ -14,9 +14,10 @@ from xraydb import guess_edge, xray_edge
 from larch.xafs import pre_edge, find_e0
 from larch.io import read_ascii, write_ascii, merge_groups, read_athena, create_athena, write_group
 import athena_project
-import palettable.colorbrewer.diverging as pld
+import palettable as pltt
 from collections import defaultdict
 from pprint import pprint
+import configparser
 
 # Constant
 """
@@ -25,8 +26,8 @@ FILE_TYPE instructions:
 '' or '.dat' for merging transmission scans
 '.txt' for plotting scans
 """
-FILE_TYPE = '.txt'
-INPUT_PATH = r'D:\Research data\SSID\202206\20220610 BMM\b31-pure NbAl'
+FILE_TYPE = '.dat'
+INPUT_PATH = r'D:\Research data\SSID\202209\20220330 ISS NbOx'
 
 # Merged Constant
 SKIP_SCANS = ['MnO2_45_16C_Charge_Mn_001']     # [] if scans are good or just add scans you want to exclude
@@ -39,18 +40,24 @@ SHOW_DATA_INFORMATION = False   # List athena parameters, such as atomic symbol,
 You could set FILE_INDEX = 0, SAMPLE_LIST = [], STANDARD_LIST = [], 
 SAMPLE_LABEL = [], ENERGY_RANGE = () as a default for your first try.
 """
-FILE_INDEX = 0  # Which file in the file list you want to plot
-SAMPLE_LIST = [10, 9, 11, 8, 7]     # [] for default or [1, 7, 5, 3] for a index list you want to plot
-STANDARD_LIST = [7]      # [] if no standards in the SAMPLE_LIST or [5, 3] in the SAMPLE_LIST become dash lines
-SAMPLE_LABEL = ['Pristine', '900C00M', '900C7p5M', '900C60M', 'Pure Nb']  # [] for default or add a specific name list
-FIGURE_SIZE = (6.4, 4.8)  # Cheng-hung uses (6, 7.5), but the default is (6.4, 4.8)
-PALETTE = pld.Spectral_4_r  # _r if you want to reverse the color sequence
+CONFIG_FILE = r"D:\Research data\SSID\202205\20220509 20210221 BMM\b28_Sc_pure_config.ini"
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE, encoding="utf8")
+
+FILE_INDEX = config.getint('samples', 'file_index')  # Which file in the file list you want to plot
+SAMPLE_LIST = eval(config['samples']['sample_list'])  # [] for default or [1, 7, 5, 3] for a index list you want to plot
+STANDARD_LIST = eval(config['samples']['standard_list'])  # [] if none or [5, 3] in the SAMPLE_LIST become dash lines
+SAMPLE_LABEL = eval(config['legends']['sample_label'])  # [] for default or add a specific name list
+FIGURE_SIZE = eval(config['format']['figure_size'])  # Cheng-Hung uses (6, 7.5), but the default is (6.4, 4.8)
+PALETTE = eval(config['format']['palette'])     # pld.Spectral_4_r  # _r if you want to reverse the color sequence
 CMAP = PALETTE.mpl_colormap     # .mpl_colormap attribute is a continuous, interpolated map
-OFFSET = 0.15  # Value you want to add to an y offset for each curve.
-ENERGY_RANGE = (18900, 19150)   # () for default, (18900, 19150) for Nb, (4425, 4625) for Sc
-ENERGY_INTERVAL = 50   # This parameter works only when you set a ENERGY_RANGE
-IF_SAVE = True
-OUTPUT_FILENAME = 'test'
+COLOR_INCREMENT = eval(config['format']['color_increment'])
+OFFSET = eval(config['format']['offset'])   # Value you want to add to an y offset for each curve.
+ENERGY_RANGE = eval(config['format']['energy_range'])   # () for default, (18900, 19150) for Nb, (4425, 4625) for Sc
+ENERGY_INTERVAL = eval(config['format']['energy_interval'])   # This parameter works only when you set a ENERGY_RANGE
+IF_SAVE = eval(config['format']['if_save'])
+OUTPUT_FILENAME = config['format']['output_filename']
+NUM_COLUMN = 1
 
 
 def main():
@@ -70,7 +77,7 @@ def main():
         print('=================================================================================')
 
         # Call Path again to grab the created athena project
-        files = Path(INPUT_PATH).glob(f'*{FILE_TYPE}')
+        files = Path(INPUT_PATH).glob(f'*.prj')
         calibrate_energy(files)
 
     elif FILE_TYPE == '.txt':
@@ -141,20 +148,21 @@ def plot_xas(files):
     print("------------------------------")
     increment = 0   # Increment for offset
     if len(SAMPLE_LIST) == 0:
-        color_idx = np.linspace(0, 1, len(file_keys)-7)   # All plots have their own color
+        color_idx = np.linspace(0, 1, len(file_keys)-7+COLOR_INCREMENT)   # All plots have their own color
         for i in range(len(file_keys)-7):   # Start from the first sample name because file key 0-6 are data information
             sample_index = i + 7
             sample_name = file_keys[sample_index]
-            if len(SAMPLE_LABEL) > i:
-                sample_label = SAMPLE_LABEL[i]
-            else:
-                sample_label = sample_name
+            sample_label = sample_name
+            # if len(SAMPLE_LABEL) > i:
+            #     sample_label = SAMPLE_LABEL[i]
+            # else:
+            #     sample_label = sample_name
             mu = getattr(file, file_keys[sample_index])
-            ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment]), label=sample_label)
+            ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment+COLOR_INCREMENT]), label=sample_label)
             increment += 1
             print('{:>3}     {}'.format(sample_index, sample_name))
     else:
-        color_idx = np.linspace(0, 1, len(SAMPLE_LIST))     # Only the plots you want have their own color
+        color_idx = np.linspace(0, 1, len(SAMPLE_LIST)+COLOR_INCREMENT)     # Only the plots you want have their own color
         for sample_index in SAMPLE_LIST:
             sample_name = file_keys[sample_index]
             if len(SAMPLE_LABEL) > SAMPLE_LIST.index(sample_index):
@@ -163,9 +171,9 @@ def plot_xas(files):
                 sample_label = sample_name
             mu = getattr(file, file_keys[sample_index])
             if sample_index in STANDARD_LIST:
-                ax1.plot(energy, mu + OFFSET * increment, '--', color=CMAP(color_idx[increment]), label=sample_label)
+                ax1.plot(energy, mu + OFFSET * increment, '--', color=CMAP(color_idx[increment+COLOR_INCREMENT]), label=sample_label)
             else:
-                ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment]), label=sample_label)
+                ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment+COLOR_INCREMENT]), label=sample_label)
             increment += 1
             print('{:>3}     {}'.format(sample_index, sample_name))
 
@@ -183,7 +191,7 @@ def plot_xas(files):
     ax1.set_xlabel(x_label, fontsize=18)
     ax1.set_ylabel(y_label, fontsize=18)
     plt.rcParams["axes.linewidth"] = 5
-    plt.legend(loc='lower right', framealpha=1, frameon=False, fontsize=14)
+    plt.legend(loc='lower right', framealpha=1, frameon=False, fontsize=14, ncol=NUM_COLUMN)
     plt.tight_layout()
     if IF_SAVE:
         output_filename = check_filename_repetition(OUTPUT_FILENAME)
@@ -374,6 +382,7 @@ def read_transmission(files):
                 if f'{sample_name}_00{mu_index - 1 + skip_times}' in SKIP_SCANS:
                     skip_times += 1
                 label = f'{sample_name}_00{mu_index - 1 + skip_times}'
+
             if mu_index > 1:
                 print(label)
                 plt.plot(energy, mu, label=label)
